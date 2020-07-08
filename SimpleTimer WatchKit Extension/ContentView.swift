65290e857a4349af12e51e39b505933ef9415ce7
vorontsov <vorontsov@nextexit.ru>
2020-07-08 19:50:23 +0300

    Initial Commit

---
+++ SimpleTimer WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleTimer WatchKit Extension
+//
+//  Created by ANDREY VORONTSOV on 08.07.2020.
+//  Copyright © 2020 ANDREY VORONTSOV. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleTimer WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  SimpleTimer WatchKit Extension
+//
+//  Created by ANDREY VORONTSOV on 08.07.2020.
+//  Copyright © 2020 ANDREY VORONTSOV. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
